f62a8e900f7125705c6a315f0692418042009907
allen2hawaii <97934930+allen2hawaii@users.noreply.github.com>
2022-03-04 11:50:39 +0000

    changed strings into template literals.

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html")
 });
 
 app.post("/", function(req, res) {
 
-  var lat = String(req.body.latInput);
-  var lon = String(req.body.lonInput);
+  const lat = String(req.body.latInput);
+  const lon = String(req.body.lonInput);
   console.log(lat, lon);
   
-  const units = "imperial";
-  const apiKey = "d1e66fd78c9b675a43f105f9d956a93c";
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + units + "&APPID=" + apiKey;
+  let units = "imperial";
+  let apiKey = "d1e66fd78c9b675a43f105f9d956a93c";
+  let url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + units + "&APPID=" + apiKey;
 
   https.get(url, function(response){
     console.log(response.statusCode);
         
         
-  response.on("data", function(data){
+    response.on("data", function(data){
     
     const weatherData = JSON.parse(data);
     const coord = weatherData.coord;
@@ -32,43 +32,24 @@ app.post("/", function(req, res) {
     const weatherDescription = weatherData.weather[0].description;
     const temp = weatherData.main.temp;
     const icon = weatherData.weather[0].icon;
-    const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
     const humidity = weatherData.main.humidity;
     const windSpeed = weatherData.wind.speed;
     const cloudiness = weatherData.clouds.all;
+    let imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
     
-    res.write("<h1>The coordinates (" + lat + ", " + lon + "),  is for the city of " + city + "." );
-    res.write("<h2>The weather in " + city + " is " + weatherDescription + ".");
-    res.write("<h2>Temperature: " + temp.toFixed(1) + " Degrees Fahrenheit");
+    res.write(`<h1>The coordinates (${lat}, ${lon}),  is the city of ${city}.`);
+    res.write(`<h2>The weather in ${city} is ${weatherDescription}.`);
+    res.write(`<h2>Temperature: ${temp.toFixed(1)}°F`);
     res.write("<img src=" + imageURL + ">");
-    res.write("<h4>Humidity: " + humidity + "%");
-    res.write("<h4>Wind Speed: " + windSpeed.toFixed(1) + " miles/hour");
-    res.write("<h4>Cloudiness: " + cloudiness + "%");
+    res.write(`<h3>Humidity: ${humidity}%`);
+    res.write(`<h3>Windspeed: ${windSpeed.toFixed(0)} mph`);
+    res.write(`<h3>Cloudiness: ${cloudiness}%`);
 
        });
    });
 })
 
-
-app.listen(process.env.PORT || 3000, function() {
-console.log ("Server is running on port")
+let port = 3000;
+app.listen(process.env.PORT || port, function() {
+  console.log (`Server is running on port ${port}! `)
 });
-    
-
-        
-       
-    
-    
-        
-    
-    
-    
-
-            
-            
-            
-            
-            
-            
-           
-     
