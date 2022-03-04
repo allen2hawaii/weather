b097ffa2aaae7e2f89eb6825e45a2bafaee324db
allen2hawaii <97934930+allen2hawaii@users.noreply.github.com>
2022-03-04 04:55:01 +0000

    Changed form to search weather by GPS coordinates Latitude/Longitude.

--- index.js
+++ index.js
@@ -6,63 +6,69 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-//displays index.html of root path
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html")
 });
 
-//invoked after hitting go in the html form
 app.post("/", function(req, res) {
-    
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var cityName = String(req.body.cityNameInput);
-        console.log(req.body.cityInput);
-    
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
-        const units = "imperial";
-        const apiKey = "d1e66fd78c9b675a43f105f9d956a93c";
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName +  "&units=" + units + "&APPID=" + apiKey;
-    
-    // this gets the data from Open WeatherPI
-    https.get(url, function(response){
-        console.log(response.statusCode);
-        
-        // gets individual items from Open Weather API
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const feelsLike = weatherData.main.feels_like;
-            const humidity = weatherData.main.humidity;
-            const windSpeed = weatherData.wind.speed;
-            const windGust = weatherData.wind.gust;
-            const sunrise = weatherData.sys.sunrise;
-            const sunset = weatherData.sys.sunset;
-            const city = weatherData.name;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-            
-            
-            
-            
-            // displays the output of the results
-            res.write("<h1>The weather in " + city + " is " + weatherDescription);
-            res.write("<img src=" + imageURL +">");
-            res.write("<h2>Temperature: " + temp.toFixed(1) + " Degrees Fahrenheit");
-            res.write("<h3>Feels Like : " + feelsLike.toFixed(1) + " Degrees Fahrenheit");
-            res.write("<h4>Humidity: " + humidity + "%");
-            res.write("<h4>Wind Speed: " + windSpeed.toFixed(1) + " miles/hour");
-            res.write("<h4>Wind Gust: " + windGust + " miles/hour");
-            res.write("<h4>Sunrise: " + sunrise);
-            res.write("<h4>Sunset: " + sunset);
-            res.send();
-        });
-    });
+  var lat = String(req.body.latInput);
+  var lon = String(req.body.lonInput);
+  console.log(lat, lon);
+  
+  const units = "imperial";
+  const apiKey = "d1e66fd78c9b675a43f105f9d956a93c";
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + units + "&APPID=" + apiKey;
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+        
+        
+  response.on("data", function(data){
+    
+    const weatherData = JSON.parse(data);
+    const coord = weatherData.coord;
+    const city = weatherData.name;
+    const weatherDescription = weatherData.weather[0].description;
+    const temp = weatherData.main.temp;
+    const icon = weatherData.weather[0].icon;
+    const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    const humidity = weatherData.main.humidity;
+    const windSpeed = weatherData.wind.speed;
+    const cloudiness = weatherData.clouds.all;
+    
+    res.write("<h1>The coordinates (" + lat + ", " + lon + "),  is for the city of " + city + "." );
+    res.write("<h2>The weather in " + city + " is " + weatherDescription + ".");
+    res.write("<h2>Temperature: " + temp.toFixed(1) + " Degrees Fahrenheit");
+    res.write("<img src=" + imageURL + ">");
+    res.write("<h4>Humidity: " + humidity + "%");
+    res.write("<h4>Wind Speed: " + windSpeed.toFixed(1) + " miles/hour");
+    res.write("<h4>Cloudiness: " + cloudiness + "%");
+
+       });
+   });
 })
 
 
-//Code will run on 3000 or any available open port
 app.listen(process.env.PORT || 3000, function() {
 console.log ("Server is running on port")
 });
+    
+
+        
+       
+    
+    
+        
+    
+    
+    
+
+            
+            
+            
+            
+            
+            
+           
+     
